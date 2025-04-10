ee414610e28bc6c559fe4c59fc6c667486bb8c18
Benny Chopra <bchopra@bsd220.org>
2025-04-10 09:31:21 -0500

    Created brick ceiling

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -153,7 +153,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(brick)
         bricks.append(brick)
     }
-    //jkhbnhdfgbvder
+    //jkhbnhdfgbvde
     func makeLoseZone () {
         loseZone = SKSpriteNode(color: .red, size: CGSize(width: frame.width, height: 50))
         loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
